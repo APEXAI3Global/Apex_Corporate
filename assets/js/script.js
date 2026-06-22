2b0855c47c2a569497de28dcd5a658165cb9b1c0
karthik13vijay <karthick.vijay@hotmail.com>
2026-06-22 13:13:30 +0530

    footer changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -352,4 +352,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
+fetch("../../footer.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch(error => console.error("Footer load error:", error));
